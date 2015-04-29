03d99bc9828bac1eea21812f7c1c2699f7d6b0a2
fc01 <910677221@qq.com>
2015-04-29 13:55:51 +0800

    ——

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # api.json  ---> web api code
+```objective-c
+[WebAPI products_list:self cate:@"" page:@"" pagesize:@"" sort:@"" onSuceess:^(Ret_products_list *ret) {
+        NSLog(@"ret.r_BrandData.count=%ld",ret.r_BrandData.count);
+        if (ret.r_BrandData.count>0) {
+            NSLog(@"ret.r_BrandData[0].r_BName=%@",ret.r_BrandData[0].r_BName);
+        }
+    } onFailed:^(enum WebAPIErrorCode errorCode, NSString *msg) {
+        NSLog(@"errorCode=%ld msg=%@",errorCode,msg);
+    }];
+```
--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -19,7 +19,10 @@
     [super viewDidLoad];
     
     [WebAPI products_list:self cate:@"" page:@"" pagesize:@"" sort:@"" onSuceess:^(Ret_products_list *ret) {
-        NSLog(@"ret.r_BrandData[0].r_BName=%@",ret.r_BrandData[0].r_BName);
+        NSLog(@"ret.r_BrandData.count=%ld",ret.r_BrandData.count);
+        if (ret.r_BrandData.count>0) {
+            NSLog(@"ret.r_BrandData[0].r_BName=%@",ret.r_BrandData[0].r_BName);
+        }
     } onFailed:^(enum WebAPIErrorCode errorCode, NSString *msg) {
         NSLog(@"errorCode=%ld msg=%@",errorCode,msg);
     }];
