1c0efb845be70db677d5ed4f197147e3ec87a46e
EWT <610893667@qq.com>
2015-04-30 11:29:02 +0800

    -

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -20,11 +20,15 @@
     
     [WebAPI products_list:self cate:@"" page:@"" pagesize:@"" sort:@"" onSuceess:^(Ret_products_list *ret) {
         NSLog(@"ret.r_BrandData.count=%ld",ret.r_BrandData.count);
-        if (ret.r_BrandData.count>0) {
+        
+        //if (ret.r_BrandData.count>0) {
             NSLog(@"ret.r_BrandData[0].r_BName=%@",ret.r_BrandData[0].r_BName);
-        }
+       // }
+        
     } onFailed:^(enum WebAPIErrorCode errorCode, NSString *msg) {
+        
         NSLog(@"errorCode=%ld msg=%@",errorCode,msg);
+        
     }];
 }
 
--- webapi/DJHttp.h
+++ webapi/DJHttp.h
@@ -3,18 +3,17 @@
 
 @interface DJHttp : NSObject
 
-//全局事件注册
-+(void)registerErrorCode:(enum WebAPIErrorCode)status block:(void(^)())block;
++(void)registerStatus:(enum WebAPIErrorCode)status block:(void(^)())block;
 
-//id + path + method 相同    取消上次
-+(void)sendRequestWithId:(id)Id
-            method:(NSString*)method
-            path:(NSString*)path
-            param:(NSDictionary*)param
-            onSuceess:(void(^)(id data))onSuceess
-            onFailed:(void(^)(enum WebAPIErrorCode errorCode,NSString* msg))onFailed;
+//id   path+method  相同取消上次
++(void)callhttpReguest:(id)Id
+                  path:(NSString*)funcName
+                method:(NSString*)method
+                 param:(NSDictionary*)param
+             onSuceess:(void(^)(NSArray* retArr))onSuceess
+              onFailed:(void(^)(enum WebAPIErrorCode status,NSString* msg))onFailed;
 
-//取消 id 所有
-+(void)cancelAllRequestWithId:(id)Id;
+//  取消 id 所有
++(void)cancelRequest:(id)Id;
 
 @end
