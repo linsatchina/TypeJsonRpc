f4fc2f69fdf74d21cca291760784db82346a0957
fc01 <910677221@qq.com>
2015-04-30 11:34:26 +0800

    _

--- webapi/DJHttp.h
+++ webapi/DJHttp.h
@@ -1,10 +1,10 @@
 #import <Foundation/Foundation.h>
-#import "WebAPIErrorCode.h"
+#import "WebAPIStatus.h"
 
 @interface DJHttp : NSObject
 
 //全局事件注册
-+(void)registerErrorCode:(enum WebAPIErrorCode)status block:(void(^)())block;
++(void)registerErrorCode:(enum WebAPIStatus)status block:(void(^)())block;
 
 //id + path + method 相同    取消上次
 +(void)sendRequestWithId:(id)Id
@@ -12,7 +12,7 @@
             path:(NSString*)path
             param:(NSDictionary*)param
             onSuceess:(void(^)(id data))onSuceess
-            onFailed:(void(^)(enum WebAPIErrorCode errorCode,NSString* msg))onFailed;
+            onFailed:(void(^)(enum WebAPIStatus errorCode,NSString* msg))onFailed;
 
 //取消 id 所有
 +(void)cancelAllRequestWithId:(id)Id;
